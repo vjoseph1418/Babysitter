bf64d57a88d0ccf9fd16cda7498e736fbacf6592
vjoseph1418@gmail.com <you@example.com>
2020-03-18 21:03:43 -0400

    Put the validation logic into a separate function

--- src/main/java/com/babysitter/PaymentCalculator.java
+++ src/main/java/com/babysitter/PaymentCalculator.java
@@ -5,12 +5,16 @@ import org.apache.commons.lang3.StringUtils;
 public class PaymentCalculator {
 
     public Integer calculate(String startTime, String endTime, String family) {
+        validateInputs(startTime, endTime, family);
+        return 0;
+    }
+
+    private void validateInputs(String startTime, String endTime, String family) {
         if(StringUtils.isBlank(startTime))
             System.out.println("Start time cannot be blank!");
         if(StringUtils.isBlank(endTime))
             System.out.println("End time cannot be blank!");
         if(StringUtils.isBlank(family))
             System.out.println("Family cannot be blank!");
-        return 0;
     }
 }
