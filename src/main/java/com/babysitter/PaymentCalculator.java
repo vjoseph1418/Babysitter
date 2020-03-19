d035010a4064b951662015968975e482bfac4e1c
vjoseph1418@gmail.com <you@example.com>
2020-03-18 20:56:09 -0400

    Added test for checking if start time is blank or not

--- src/main/java/com/babysitter/PaymentCalculator.java
+++ src/main/java/com/babysitter/PaymentCalculator.java
@@ -1,7 +1,12 @@
 package com.babysitter;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class PaymentCalculator {
+
     public Integer calculate(String startTime, String endTime, String family) {
+        if(StringUtils.isBlank(startTime))
+            System.out.println("Start time cannot be blank!");
         return 0;
     }
 }
--- src/test/java/com/babysitter/PaymentCalculatorTest.java
+++ src/test/java/com/babysitter/PaymentCalculatorTest.java
@@ -1,13 +1,31 @@
 package com.babysitter;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class PaymentCalculatorTest {
 
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
     @Test
-    public void whenCalculateIsCalledTotalPayIsZero() {
+    public void whenCalculateIsCalledTotalPayIsZero() throws Exception {
         PaymentCalculator paymentCalculator = new PaymentCalculator();
         Assert.assertEquals(0, paymentCalculator.calculate("18:00", "19:00", "A"), 0.01);
+    }
+
+    @Test
+    public void whenCalculateIsCalledWithEmptyParametersAnExceptionIsThrown() throws Exception {
+        PaymentCalculator paymentCalculator = new PaymentCalculator();
+        paymentCalculator.calculate("", "19:00", "A");
+        Assert.assertEquals("Start time cannot be blank!", outputStream.toString().trim());
     }
 }
