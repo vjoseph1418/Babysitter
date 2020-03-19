4d716afaf5c5c204d7a0adf0ff886fdcbcc77c99
vjoseph1418@gmail.com <you@example.com>
2020-03-18 21:05:47 -0400

    Removed the unnecessary exception

--- src/test/java/com/babysitter/PaymentCalculatorTest.java
+++ src/test/java/com/babysitter/PaymentCalculatorTest.java
@@ -17,27 +17,27 @@ public class PaymentCalculatorTest {
     }
 
     @Test
-    public void whenCalculateIsCalledThenTotalPayIsZero() throws Exception {
+    public void whenCalculateIsCalledThenTotalPayIsZero() {
         PaymentCalculator paymentCalculator = new PaymentCalculator();
         Assert.assertEquals(0, paymentCalculator.calculate("18:00", "19:00", "A"), 0.01);
     }
 
     @Test
-    public void whenCalculateIsCalledWithABlankStartTimeThenAnErrorMessageIsPrinted() throws Exception {
+    public void whenCalculateIsCalledWithABlankStartTimeThenAnErrorMessageIsPrinted() {
         PaymentCalculator paymentCalculator = new PaymentCalculator();
         paymentCalculator.calculate("", "19:00", "A");
         Assert.assertEquals("Start time cannot be blank!", outputStream.toString().trim());
     }
 
     @Test
-    public void whenCalculateIsCalledWithABlankEndTimeThenAnErrorMessageIsPrinted() throws Exception {
+    public void whenCalculateIsCalledWithABlankEndTimeThenAnErrorMessageIsPrinted() {
         PaymentCalculator paymentCalculator = new PaymentCalculator();
         paymentCalculator.calculate("18:00", "", "A");
         Assert.assertEquals("End time cannot be blank!", outputStream.toString().trim());
     }
 
     @Test
-    public void whenCalculateIsCalledWithABlankFamilyThenAnErrorMessageIsPrinted() throws Exception {
+    public void whenCalculateIsCalledWithABlankFamilyThenAnErrorMessageIsPrinted() {
         PaymentCalculator paymentCalculator = new PaymentCalculator();
         paymentCalculator.calculate("18:00", "19:00", "");
         Assert.assertEquals("Family cannot be blank!", outputStream.toString().trim());
